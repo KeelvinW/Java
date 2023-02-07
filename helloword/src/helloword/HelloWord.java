d50d57d0592a406382bb5829e91d9ac31229b105
kelvin Wesley <keelvin.w@hotmail.com>
2023-02-07 12:00:52 -0300

    hello word

---
+++ helloword/src/helloword/HelloWord.java
@@ -0,0 +1,13 @@
+package helloword;
+
+public class HelloWord {
+
+	public static void main(String[] args) {
+		
+		System.out.println("hello Word!");
+		System.out.println("Eu sou um Dev de qualidade");
+		
+		
+	}
+
+}
